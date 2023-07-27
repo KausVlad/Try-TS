e9733e9bee1b6aaff9b77a547a61fb91511d6a05
KausVlad <kausvirtual@gmail.com>
2023-07-27 19:30:46 +0300

    basic type

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var test = 'Hello World';
 console.log(test);
-function double(X) {
-    return X * 2;
+function double(x) {
+    return x * 2;
 }
+function double2(x) {
+    console.log(x * 2);
+}
+var triple = function (x) { return x * 3; };
+var x = 10;
+x = 'Hello';
--- index.ts
+++ index.ts
@@ -1,7 +1,26 @@
-let test: string = 'Hello World';
+const test: string = 'Hello World';
 
 console.log(test);
 
-function double(X: number): number {
-  return X * 2;
+function double(x: number): number {
+  return x * 2;
 }
+
+function double2(x: number): void {
+  console.log(x * 2);
+}
+
+const triple = (x: number) => x * 3;
+
+let x = 10;
+// x = 'Hello';
+// let y;
+let y: number;
+y = 10;
+
+let xFunc: (x: number) => number;
+xFunc = (xx) => xx * 2;
+
+const names: string[] = ['John', 'Paul', 'George', 'Ringo'];
+const numbers: number[] = [1, 2, 3];
+// const neverW = []; //! never type is not good
