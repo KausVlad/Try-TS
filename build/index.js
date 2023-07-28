9c05828f4f32cdc70457f3867ec06dd9e5f1ac6a
KausVlad <kausvirtual@gmail.com>
2023-07-28 23:59:43 +0300

    TSconfig

---
+++ build/index.js
@@ -0,0 +1,25 @@
+"use strict";
+const test = 'Hello World';
+console.log(test);
+function double(x) {
+    return x * 2;
+}
+function double2(x) {
+    console.log(x * 2);
+}
+const triple = (x) => x * 3;
+let x = 10;
+// x = 'Hello';
+// let y;
+let y;
+y = 10;
+let xFunc;
+xFunc = (xx) => xx * 2;
+const names = ['John', 'Paul', 'George', 'Ringo'];
+const numbers = [1, 2, 3];
+const numbersX = [1, 2, 3];
+// const neverW = []; //! never type is not good
+const personTuple = ['John', 10, true];
+const nothing = undefined;
+const nothingX = null;
+//# sourceMappingURL=index.js.map
